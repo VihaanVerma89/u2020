6e7299b1716d1377c1135b4ab758975683ee809f
Vihaan <vihaanverma21@gmail.com>
2018-04-01 01:52:32 +0530

    first commit

---
+++ app/src/main/java/com/jakewharton/u2020/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jakewharton.u2020.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jakewharton.u2020.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
